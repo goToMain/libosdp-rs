f7def9df823247c7ef4cfb67f7204cc8c8c3578f
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2023-12-10 18:29:53 +0100

    Fix crate build failure due to no-git
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- build.rs
+++ build.rs
@@ -1,52 +1,21 @@
 use std::{
-    path::{PathBuf, Path},
-    ffi::OsString,
-    io::{ErrorKind, Error},
-    process::Command, borrow::BorrowMut, str::FromStr,
+    borrow::BorrowMut,
+    path::{Path, PathBuf},
+    process::Command,
 };
 
 use anyhow::Context;
 type Result<T> = anyhow::Result<T, anyhow::Error>;
 
-fn get_repo_root() -> std::io::Result<String> {
-    let path = std::env::current_dir()?;
-    let mut path_ancestors = path.as_path().ancestors();
-
-    while let Some(p) = path_ancestors.next() {
-        let has_git = std::fs::read_dir(p)?
-            .into_iter()
-            .any(|p| {
-            let ent = p.unwrap();
-            if ent.file_type().unwrap().is_dir() {
-                ent.file_name() == OsString::from(".git")
-            } else {
-                false
-            }
-        });
-        if has_git {
-            return Ok(PathBuf::from(p).into_os_string().into_string().unwrap())
-        }
-    }
-
-    let mut path_ancestors = path.as_path().ancestors();
-    while let Some(p) = path_ancestors.next() {
-        let has_cargo =
-            std::fs::read_dir(p)?
-            .into_iter()
-            .any(|p| p.unwrap().file_name() == OsString::from("Cargo.lock"));
-        if has_cargo {
-            return Ok(PathBuf::from(p).into_os_string().into_string().unwrap())
-        }
-    }
-
-    Err(Error::new(ErrorKind::NotFound, "Unable to determine repo root"))
-}
-
 fn path_join(root: &str, path: &str) -> String {
-    Path::new(root).join(path).into_os_string().into_string().unwrap()
+    Path::new(root)
+        .join(path)
+        .into_os_string()
+        .into_string()
+        .unwrap()
 }
 
-fn configure_file(path: &str, transforms: Vec<(&str, &str)>) -> Result<()>{
+fn configure_file(path: &str, transforms: Vec<(&str, &str)>) -> Result<()> {
     let mut contents = std::fs::read_to_string(path)?;
 
     for (from, to) in transforms {
@@ -79,13 +48,14 @@ struct GitInfo {
 
 impl GitInfo {
     pub fn new() -> Result<Self> {
-        let diff = match exec_cmd(vec![ "git", "diff", "--quiet", "--exit-code" ]) {
+        let diff = match exec_cmd(vec!["git", "diff", "--quiet", "--exit-code"]) {
             Ok(_) => "",
             Err(_) => "+",
         };
         Ok(GitInfo {
             branch: exec_cmd(vec!["git", "rev-parse", "--abbrev-ref", "HEAD"])?,
-            tag: exec_cmd(vec![ "git", "describe", "--exact-match", "--tags" ]).unwrap_or("".to_owned()),
+            tag: exec_cmd(vec!["git", "describe", "--exact-match", "--tags"])
+                .unwrap_or("".to_owned()),
             diff: diff.to_owned(),
             rev: exec_cmd(vec!["git", "log", "--pretty=format:'%h'", "-n", "1"])?,
             root: exec_cmd(vec!["git", "rev-parse", "--show-toplevel"])?,
@@ -96,81 +66,32 @@ impl GitInfo {
 fn generate_osdp_build_headers(out_dir: &str) -> Result<()> {
     // Add an empty file as we don't
     let data = "#define OSDP_EXPORT";
-    std::fs::write(
-        path_join(&out_dir, "osdp_export.h"),
-        data
-    ).context("Failed to create osdp_export.h")?;
+    std::fs::write(path_join(&out_dir, "osdp_export.h"), data)
+        .context("Failed to create osdp_export.h")?;
 
     let git = GitInfo::new()?;
     let src = "vendor/src/osdp_config.h.in";
     let dest = path_join(&out_dir, "osdp_config.h");
-    std::fs::copy(&src, &dest)
-        .context(format!("Failed: copy {src} -> {dest}"))?;
-    configure_file(&dest, vec![
+    std::fs::copy(&src, &dest).context(format!("Failed: copy {src} -> {dest}"))?;
+    configure_file(
+        &dest,
+        vec![
             ("PROJECT_VERSION", env!("CARGO_PKG_VERSION")),
-        ("PROJECT_NAME", format!("{}-rust", env!("CARGO_PKG_NAME")).as_str()),
+            (
+                "PROJECT_NAME",
+                format!("{}-rust", env!("CARGO_PKG_NAME")).as_str(),
+            ),
             ("GIT_BRANCH", git.branch.as_str()),
             ("GIT_REV", git.rev.as_ref()),
             ("GIT_TAG", git.tag.as_ref()),
             ("GIT_DIFF", git.diff.as_ref()),
             ("REPO_ROOT", git.root.as_ref()),
-    ])
-}
-
-fn dirname_mkdir(path: &Path) {
-    let mut dir = PathBuf::from(path);
-    dir.pop();
-    std::fs::create_dir_all(&dir).unwrap();
-}
-
-fn vendor_sources(root: &str, vendor: &str) -> Result<()> {
-    let sources = vec![
-        "utils/include/utils/assert.h",
-        "utils/src/list.c",
-        "utils/include/utils/list.h",
-        "utils/src/queue.c",
-        "utils/include/utils/queue.h",
-        "utils/src/slab.c",
-        "utils/include/utils/slab.h",
-        "utils/src/utils.c",
-        "utils/include/utils/utils.h",
-        "utils/src/logger.c",
-        "utils/include/utils/logger.h",
-        "utils/src/disjoint_set.c",
-        "utils/include/utils/disjoint_set.h",
-        "include/osdp.h",
-        "src/osdp_common.c",
-        "src/osdp_common.h",
-        "src/osdp_config.h.in",
-        "src/osdp_phy.c",
-        "src/osdp_sc.c",
-        "src/osdp_file.c",
-        "src/osdp_file.h",
-        "src/osdp_pd.c",
-        "src/osdp_cp.c",
-        "src/crypto/tinyaes_src.c",
-        "src/crypto/tinyaes_src.h",
-        "src/crypto/tinyaes.c",
-    ];
-    let root = PathBuf::from_str(root)?;
-    for file in sources {
-        let src = root.clone().join(Path::new(&file));
-        let mut dest = PathBuf::from(vendor);
-        dest.push(file);
-        dirname_mkdir(&dest);
-        // Tell Cargo that if the given file changes, to rerun this build script.
-        println!("cargo:rerun-if-changed={}", src.display());
-        std::fs::copy(&src, &dest)?;
-    }
-    Ok(())
+        ],
+    )
 }
 
 fn main() -> Result<()> {
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    let repo_root = get_repo_root()?;
-    let vendor = concat!(env!("CARGO_MANIFEST_DIR"), "/vendor").to_owned();
-
-    let _ = vendor_sources(&repo_root, &vendor);
 
     generate_osdp_build_headers(&out_dir)?;
 
@@ -197,7 +118,7 @@ fn main() -> Result<()> {
         "vendor/src/osdp_pd.c",
         "vendor/src/osdp_cp.c",
         "vendor/src/crypto/tinyaes_src.c",
-        "vendor/src/crypto/tinyaes.c"
+        "vendor/src/crypto/tinyaes.c",
     ];
 
     for file in source_files {
@@ -219,12 +140,12 @@ fn main() -> Result<()> {
     build.compile("libosdp.a");
 
     let bindings = bindgen::Builder::default()
-        .use_core()
         .header("vendor/include/osdp.h")
         .generate()
         .context("Unable to generate bindings")?;
 
     let out_path = PathBuf::from(out_dir);
-    bindings.write_to_file(out_path.join("bindings.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
         .context("Couldn't write bindings!")
 }
