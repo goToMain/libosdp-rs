e2a52a40d40e9e593b0daa11bb5a2939bb8391c2
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2024-02-17 22:16:27 +0100

    Move some more stuff from libosdp
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

---
+++ scripts/make-release.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+#  Copyright (c) 2024 Siddharth Chandrasekaran <sidcha.dev@gmail.com>
+#
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+usage() {
+	cat >&2<<----
+	LibOSDP release helper
+
+	OPTIONS:
+	  -c, --component	Compoenent to release (can be one of libosdp, libosdp-sys, osdpctl)
+	  --patch		Release version bump type: patch (default)
+	  --major		Release version bump type: major
+	  --minor		Release version bump type: minor
+	  -h, --help		Print this help
+	---
+}
+
+function caro_inc_version() {
+	dir=$1
+	inc=$2
+	perl -pi -se '
+	if (/^version = "(\d+)\.(\d+)\.(\d+)"$/) {
+		$maj=$1; $min=$2; $pat=$3;
+		if ($major) { $maj+=1; $min=0; $pat=0; }
+		if ($minor) { $min+=1; $pat=0; }
+		$pat+=1 if $patch;
+		$_="version = \"$maj.$min.$pat\"\n"
+	}' -- -$inc $dir/Cargo.toml
+}
+
+function do_cargo_release() {
+	crate=$1
+	inc=$2
+	caro_inc_version $crate $inc
+	version=$(perl -ne 'print $1 if (/^version = "(.+)"$/)' $dir/Cargo.toml)
+	git add $dir/Cargo.toml &&
+	git commit -s -m "$crate: Release v$version" &&
+	git tag "$crate-v$version" -a -m "Release $version"
+}
+
+function do_release() {
+	case $1 in
+	libosdp-sys) do_cargo_release "libosdp-sys" $2 ;;
+	libosdp) do_cargo_release "libosdp" $2 ;;
+	osdpctl) do_cargo_release "osdpctl" $2 ;;
+	esac
+}
+
+INC="patch"
+COMPONENT="libosdp"
+while [ $# -gt 0 ]; do
+	case $1 in
+	-c|--componenet)	COMPONENT=$2; shift;;
+	--patch)		INC="patch";;
+	--major)		INC="major";;
+	--minor)		INC="minor";;
+	-h|--help)             usage; exit 0;;
+	*) echo -e "Unknown option $1\n"; usage; exit 1;;
+	esac
+	shift
+done
+
+do_release $COMPONENT $INC
+
