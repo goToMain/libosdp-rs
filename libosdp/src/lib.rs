f0f64be0a570832c43da49ce5fd501abceca7e16
Sympatron GmbH <35803463+Sympatron@users.noreply.github.com>
2024-09-27 23:35:01 +0200

    Remove once_cell dependency

--- libosdp/src/lib.rs
+++ libosdp/src/lib.rs
@@ -82,14 +82,10 @@ pub use pdcap::*;
 pub use pdid::*;
 pub use pdinfo::*;
 
-#[cfg(feature = "arc")]
-use alloc::sync::Arc;
 #[allow(unused_imports)]
 use alloc::{
     borrow::ToOwned, boxed::Box, ffi::CString, format, str::FromStr, string::String, vec, vec::Vec,
 };
-#[cfg(feature = "arc")]
-use once_cell::sync::Lazy;
 #[cfg(feature = "std")]
 use thiserror::Error;
 
@@ -218,26 +214,16 @@ fn cstr_to_string(s: *const ::core::ffi::c_char) -> String {
     s.to_str().unwrap().to_owned()
 }
 
-#[cfg(feature = "arc")]
-static VERSION: Lazy<Arc<String>> = Lazy::new(|| {
-    let s = unsafe { libosdp_sys::osdp_get_version() };
-    Arc::new(cstr_to_string(s))
-});
-
-#[cfg(feature = "arc")]
-static SOURCE_INFO: Lazy<Arc<String>> = Lazy::new(|| {
-    let s = unsafe { libosdp_sys::osdp_get_source_info() };
-    Arc::new(cstr_to_string(s))
-});
-
 /// Get LibOSDP version
-#[cfg(feature = "arc")]
-pub fn get_version() -> String {
-    VERSION.as_ref().clone()
+pub fn get_version() -> &'static str {
+    let s = unsafe { libosdp_sys::osdp_get_version() };
+    let s = unsafe { core::ffi::CStr::from_ptr(s) };
+    s.to_str().unwrap()
 }
 
 /// Get LibOSDP source info string
-#[cfg(feature = "arc")]
-pub fn get_source_info() -> String {
-    SOURCE_INFO.as_ref().clone()
+pub fn get_source_info() -> &'static str {
+    let s = unsafe { libosdp_sys::osdp_get_source_info() };
+    let s = unsafe { core::ffi::CStr::from_ptr(s) };
+    s.to_str().unwrap()
 }
