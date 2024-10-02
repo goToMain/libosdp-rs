3c36def735cb56a5ffea996db646aca2c1b17b2a
Sympatron GmbH <35803463+Sympatron@users.noreply.github.com>
2024-10-02 12:13:44 +0200

    Wrapper for osdp_pd_info_t to handle dropping of it's fields after use automatically

--- libosdp/src/pd.rs
+++ libosdp/src/pd.rs
@@ -57,10 +57,9 @@ where
     trampoline::<F>
 }
 
-fn pd_setup(mut info: PdInfo) -> Result<*mut c_void> {
-    let info_struct = info.as_struct();
-    let ctx = unsafe { libosdp_sys::osdp_pd_setup(&info_struct) };
-    crate::drop_osdp_pd_info(info_struct);
+fn pd_setup(info: PdInfo) -> Result<*mut c_void> {
+    let info: crate::OsdpPdInfoHandle = info.into();
+    let ctx = unsafe { libosdp_sys::osdp_pd_setup(&*info) };
     if ctx.is_null() {
         Err(OsdpError::Setup)
     } else {
